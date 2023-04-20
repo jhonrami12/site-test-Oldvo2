5290ce6a487567d346c8c651de01747f47e93a16
jhonrami12 <jhonrami12@gmail.com>
2023-04-20 08:39:24 -0500

    some adjustment after removing tailwinds

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { HomeComponent } from './home.component';
 import { NavbarModule } from '../navbar/navbar.module';
-import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -13,7 +14,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, NavbarModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, NavbarModule, RouterModule.forChild(routes), ButtonModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
